5d715b2a18e07be089e9e067c0f901a26fc231ce
Chen, Yi-Han a <yihan.a.chen@rakuten.com>
2020-03-08 10:34:11 +0900

    fix template

--- reactive-officer/src/main/java/com/reactive/reactiveofficer/entity/Rank.java
+++ reactive-officer/src/main/java/com/reactive/reactiveofficer/entity/Rank.java
@@ -1,13 +1,4 @@
-package com.reactive.reactiveofficer.entity;/*
- * Copyright (c) Rakuten, Inc. All Rights Reserved.
- *
- * This program is the information assets which are handled as "Strictly Confidential".
- * Permission of Use is only admitted in Rakuten Inc Development Department.
- * If you don't have permission,
- * MUST not be published, broadcast, rewritten for broadcast or publication or redistributed
- * directly or indirectly in any medium.
- *
- */
+package com.reactive.reactiveofficer.entity;
 
 public enum Rank {
     ENSIGN, LIEUTENANT, COMMANDER, CAPTAIN, COMMODORE, ADMIRAL
