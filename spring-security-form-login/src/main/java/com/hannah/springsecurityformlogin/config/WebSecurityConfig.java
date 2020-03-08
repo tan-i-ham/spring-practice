38b0ac272a1865c82f6970a825f991fc8d2b999c
Chen, Yi-Han a <yihan.a.chen@rakuten.com>
2020-03-08 10:14:18 +0900

    fix copyright

--- spring-security-form-login/src/main/java/com/hannah/springsecurityformlogin/config/WebSecurityConfig.java
+++ spring-security-form-login/src/main/java/com/hannah/springsecurityformlogin/config/WebSecurityConfig.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) Rakuten, Inc. All Rights Reserved.
- *
- * This program is the information assets which are handled as "Strictly Confidential".
- * Permission of Use is only admitted in Rakuten Inc Development Department.
- * If you don't have permission,
- * MUST not be published, broadcast, rewritten for broadcast or publication or redistributed
- * directly or indirectly in any medium.
- *
- */
 package com.hannah.springsecurityformlogin.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- spring-security-form-login/src/main/java/com/hannah/springsecurityformlogin/entity/Role.java
+++ spring-security-form-login/src/main/java/com/hannah/springsecurityformlogin/entity/Role.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) Rakuten, Inc. All Rights Reserved.
- *
- * This program is the information assets which are handled as "Strictly Confidential".
- * Permission of Use is only admitted in Rakuten Inc Development Department.
- * If you don't have permission,
- * MUST not be published, broadcast, rewritten for broadcast or publication or redistributed
- * directly or indirectly in any medium.
- *
- */
 package com.hannah.springsecurityformlogin.entity;
 
 import javax.persistence.Column;
@@ -15,8 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.util.List;
